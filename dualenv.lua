30ecaee738d00dddb2a11534c66e30055b0a4eb3
TsT <tst2005@gmail.com>
2015-09-29 21:19:36 +0200

    more comment and tests

--- dualenv.lua
+++ dualenv.lua
@@ -1,5 +1,5 @@
 
-local FORCE_NIL = {}
+local FORCE_NIL = {} -- special uniq value to remember what is removed : do not restore the original value
 local function newdualenv(ro, rw)
 	rw = rw or {}
 
@@ -7,7 +7,7 @@ local function newdualenv(ro, rw)
 	mt.__index = function(_self, k)
 		local v = rw[k]
 		if v ~= nil then
-			if v == FORCE_NIL then
+			if v == FORCE_NIL then -- do not return the ro[k] value
 				return nil
 			end
 			return v
@@ -16,13 +16,13 @@ local function newdualenv(ro, rw)
 	end
 
 	mt.__newindex = function(_self, k, v)
-		if v == nil and ro[k] ~= nil then
-			rw[k] = FORCE_NIL
+		if v == nil and ro[k] ~= nil then -- if the value was nil and something exists in the ro table
+			rw[k] = FORCE_NIL -- remember the nil write with the special value
 		else
 			rw[k] = v
 		end
 	end
-	mt.__metatable = false -- locked
+	mt.__metatable = false -- basic security, locking the metatable
 
 	local t = setmetatable({}, mt)
 	return t, rw
--- test.sharedenv.lua
+++ test.sharedenv.lua
@@ -10,16 +10,42 @@ local config = {
 local t1, rw1, s1 = new("mod1", _G, config["mod1"])
 local t2, rw2, s2 = new("mod2", _G, config["mod2"])
 local t3, rw3, s3 = new("mod3", _G, config["mod3"])
+-- rw[123] and s[123] are used for debug/test
 
 assert( rw1 == rw2 )
 assert( rw2 ~= rw3 )
 
+
+-- the sandboxes will use the t[123] as virtual _G env
+
+-- each env are different
 assert( t1 ~= t2 and t2 ~= t3)
 
+-- When you write something in t1 ...
 t1.aa = 123
+
+-- ... it was shared on t2 ...
 assert(t2.aa == 123)
 
+-- ... but not on t3.
 assert(t3.aa == nil)
+
+-- all t1/t2/t3 have the same print function (coming from the native _G.print)
+assert( t1.print == t2.print and t2.print == t3.print)
+
+-- but we should overwrite it
+t1.print = function() return "no print!" end
+assert(t1.print == t2.print) -- it was shared with t2
+assert(t3.print == _G.print) -- but t3.print still the native one
+
+t3.print = nil -- but we should also drop it
+assert(t3.print == nil)
+assert( type(_G.print) == "function") -- the native one still exists.
+
+assert( type(t1.print) == "function" )
+assert( t2.print() == "no print!")
+
+
 
 
 
